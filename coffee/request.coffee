5dae91b84416d9af4f151d652f651317bbe7dcc2
Blake Blackshear <blakeb@blakeshome.com>
2011-07-06 19:44:13 -0500

    Added MIT License and updated request to be a one liner

--- coffee/request.coffee
+++ coffee/request.coffee
@@ -1,12 +1,33 @@
-class Request
-	constructor: ->
-		@xhr = new XMLHttpRequest()
-	send: (options) ->
-		@xhr.open options.method, options.url, true, options.username or null, options.password or null
+###
+Copyright (c) 2011 by Blake Blackshear
 
-		@xhr.send options.data or null
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+###
+
+class Request
+	constructor: (options) ->
+		xhr = new XMLHttpRequest()
+		xhr.open options.method, options.url, true, options.username or null, options.password or null
+
+		xhr.send options.data or null
 
 		if options.callback?
-			@xhr.onreadystatechange =>
-				if @xhr.readyState is 4
-					options.callback @xhr
+			xhr.onreadystatechange ->
+				if xhr.readyState is 4
+					options.callback xhr
--- src/request.js
+++ src/request.js
@@ -1,21 +1,39 @@
 (function() {
-  var Request;
-  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+  /*
+  Copyright (c) 2011 by Blake Blackshear
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+  
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+  
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+  */  var Request;
   Request = (function() {
-    function Request() {
-      this.xhr = new XMLHttpRequest();
-    }
-    Request.prototype.send = function(options) {
-      this.xhr.open(options.method, options.url, true, options.username || null, options.password || null);
-      this.xhr.send(options.data || null);
+    function Request(options) {
+      var xhr;
+      xhr = new XMLHttpRequest();
+      xhr.open(options.method, options.url, true, options.username || null, options.password || null);
+      xhr.send(options.data || null);
       if (options.callback != null) {
-        return this.xhr.onreadystatechange(__bind(function() {
-          if (this.xhr.readyState === 4) {
-            return options.callback(this.xhr);
+        xhr.onreadystatechange(function() {
+          if (xhr.readyState === 4) {
+            return options.callback(xhr);
           }
-        }, this));
+        });
       }
-    };
+    }
     return Request;
   })();
 }).call(this);
